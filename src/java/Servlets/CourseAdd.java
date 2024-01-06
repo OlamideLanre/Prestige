7852f4c0fc3c0d23759d49a4b8d6d4c38d512fb4
Ola <lanreprincess@gmail.com>
2024-01-06 18:54:16 +0100

    Modified course UI, servlet to insert into db

---
+++ src/java/Servlets/CourseAdd.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+/**
+ *
+ * @author TM
+ */
+@WebServlet(name = "CourseAdd", urlPatterns = {"/CourseAdd"})
+public class CourseAdd extends HttpServlet {
+      private static final String JDBC_URL="jdbc:mysql://localhost:3306/realworlddb";
+    private static final String JDBC_USER="root";
+    private static final String JDBC_PASSWORD="";
+     
+  
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            try {
+                 Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection con= DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD);
+                System.out.println("Con Success");
+                
+                String courseInsert="INSERT INTO Course (Name, deptID) VALUES (?,?)";
+                
+                String courseName= request.getParameter("coursename");
+                int cDeptID= Integer.valueOf(request.getParameter("deptID"));
+                
+                try(PreparedStatement c=con.prepareStatement(courseInsert)) {
+                    if (!courseName.isEmpty() && cDeptID != 0) {
+                          c.setString(1, courseName);
+                          c.setInt(2,cDeptID );
+                    
+                          c.executeUpdate();
+                    response.sendRedirect("viewcoursepage.html");
+                    }else{
+                        out.write("failed");
+                    }
+                  
+                } catch (SQLException ex) {
+                    System.out.println(ex);
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
